6ebfff7877725509dbb4834e97cf3a10d23afa37
heyankit78 <ghankit78@gmail.com>
2022-01-31 13:25:56 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -16,7 +16,7 @@ const News = (props) => {
 }
   const updateNews = async(pageNo) => {
     props.setProgress(10)
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=b905920349874b91af5f859dc72978c5&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=27f7284da9ff472bb41909c84416b0d9&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true)
     let data = await fetch(url);
     props.setProgress(30)
